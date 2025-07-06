348321af5325f0cfbb30d343bb0b6c819a13299f
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-07-06 09:14:36 +0530

    create-wrapper-for-error-handler-in-controller

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -1,10 +1,12 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
+import { asyncWrapper } from "../common/utils/errorHandlerWrapper";
 
 const router = express.Router();
+
 const categoryService = new CategoryService();
 
 // Here we are injecting the service into the controller
@@ -17,9 +19,12 @@ const categoryController = new CategoryController(categoryService, logger);
 router.post(
     "/",
     categoryValidator,
-    async (req: Request, res: Response, next: NextFunction) => {
-        await categoryController.create(req, res, next);
-    },
+    // async (req: Request, res: Response, next: NextFunction) => {
+    //     await categoryController.create(req, res, next);
+    // },
+
+    // Using async wrapper
+    asyncWrapper(categoryController.create),
 );
 
 export default router;
